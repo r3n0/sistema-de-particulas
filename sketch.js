6f609a53e65725358e9dd66be154faa019f57fc9
René Martínez <hola@r3n0.com>
2022-11-23 13:02:16 -0500

    se agregaron vectores al comportamiento de las partículas

--- sketch.js
+++ sketch.js
@@ -1,7 +1,52 @@
+let nPelotas = 1000;
+let pelotas = [];
+
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+	createCanvas(windowWidth, windowHeight);
+	for (let i = 0; i < nPelotas; i++) {
+		pelotas[i] = new RandomWalek(i);
+	}
 }
 
 function draw() {
-  circle(mouseX, mouseY, 20);
+	for (let i = 0; i < nPelotas; i++) {
+		pelotas[i].update();
+		pelotas[i].display();
+	}
+}
+
+// -------------------------------
+// ----------- Classes -----------
+// -------------------------------
+
+// ------ Random Walker -------------
+class RandomWalek {
+	constructor(_name) {
+		this.red = random(0, 255);
+		this.green = random(100, 255);
+		this.blue = random(100, 150);
+
+		this.t = 0;
+		this.tSpeed = random(0.5);
+		this.noiseShift = random(1000);
+
+		this.pos = createVector(width / 2, height / 2);
+		this.speed = createVector(random(-2, 2), random(-2, 2));
+		this.diametro = random(5, 15);
+		print('Hola! soy la pelota ' + this.name);
+	}
+	update(_t) {
+		this.speed.rotate(
+			map(noise(this.t + this.noiseShift), 0, 1, -0.5, 0.5)
+		);
+		this.pos.add(this.speed);
+
+		this.t += this.tSpeed;
+	}
+	display() {
+		stroke('rgba(0,0,0,.2)');
+		strokeWeight(3);
+		fill(this.red, this.green, this.blue);
+		ellipse(this.pos.x, this.pos.y, this.diametro, this.diametro);
+	}
 }
